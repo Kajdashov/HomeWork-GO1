4febda24f4fa911279aa7d59d2a23536d1a258bf
Vladimir <Kajdashov@gmail.com>
2021-11-06 14:01:39 +0300

    Home Кайдашов

---
+++ HW Lesson 2/Lesson2Ex1.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a, b int
+
+	fmt.Print("Введите первое число: ")
+	fmt.Scanln(&a)
+
+	fmt.Print("Введите второе число: ")
+	fmt.Scanln(&b)
+
+	var c = a * b
+	fmt.Println(c)
+
+}
